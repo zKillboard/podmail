72b8da38eb36c624c278ff08a84c0e3ff258eb03
Clinton Weiss <cvweiss@gmail.com>
2018-07-18 15:03:20 +0000

    use redis for access token cache

--- classes/SSO.php
+++ classes/SSO.php
@@ -4,11 +4,6 @@ namespace podmail;
 
 class SSO
 {
-    public static function cleanup($db)
-    {
-        $db->delete('access_tokens', ['expires' => ['$lte' => time()]]);
-    }
-
     public static function getAccessToken(array $config, int $char_id, string $refresh_token, \cvweiss\Guzzler $guzzler, string $success, string $fail, array $params = [])
     {
         $params['char_id'] = $char_id;
@@ -16,17 +11,16 @@ class SSO
         $params['success'] = $success;
         $params['refresh_token'] = $refresh_token;
 
-        // Do we have a cached version?
-        $db = $config['db'];
-        self::cleanup($db);
-
-        $row = $db->queryDoc('access_tokens', ['character_id' => $char_id, 'refresh_token' => $refresh_token]);
-        if (isset($row['access_token'])) {
-            $raw = json_encode(['access_token' => $row['access_token']]);
+        // Do we have a cached access token?
+        $redis = $config['redis'];
+        $access_token = $redis->get("podmail:access_token:$char_id:$refresh_token");
+        if ($access_token != null) {
+            $raw = json_encode(['access_token' => $access_token]);
             $success($guzzler, $params, $raw);
             return;
         }
 
+        // Not cached...
         $url = 'https://login.eveonline.com/oauth/token';
         $headers = ['Authorization' =>'Basic ' . base64_encode($config['ccp']['clientID'] . ':' . $config['ccp']['secretKey']), "Content-Type" => "application/json"];
         $guzzler->call($url, '\podmail\SSO::accessTokenFetchComplete', $fail, $params, $headers, 'POST', json_encode(['grant_type' => 'refresh_token', 'refresh_token' => $refresh_token]));
@@ -34,10 +28,14 @@ class SSO
 
     public static function accessTokenFetchComplete($guzzler, $params, $content)
     {
+        $char_id = $params['char_id'];
+        $refresh_token = $params['refresh_token'];
+        $redis = $params['config']['redis'];
+
         $json = json_decode($content, true);
         $access_token = $json['access_token'];
-        $ttl = $json['expires_in'];
-        $params['config']['db']->insert('access_tokens', ['character_id' => $params['char_id'], 'refresh_token' => $params['refresh_token'], 'access_token' => $access_token, 'expires' => (time() + $ttl - 600)]);
+
+        $redis->setex("podmail:access_token:$char_id:$refresh_token", 600, $access_token);
         $params['success']($guzzler, $params, $content);
     }
 
@@ -48,11 +46,18 @@ class SSO
         if (@$json['error_description'] == 'The refresh token is expired.') {
             $char_id = $params['char_id'];
             $db->delete('scopes', ['character_id' => $char_id]);
-            $db->delete('access_tokens', ['character_id' => $char_id]);
             $db->delete('mails', ['owner' => $char_id]);
             Log::log("Purged $char_id for not having a valid refresh_token");
         } else {
-            Log::log("SSO: " . $ex->getCode() . " " . $ex->getMessage());
+            switch ($ex->getCode()) {
+                case 403:
+                case 502: 
+                case 503:
+                    // Do nothing
+                    break;
+                default:
+                    Log::log("SSO: " . $ex->getCode() . " " . $ex->getMessage());
+            }
         }
     }
 }
